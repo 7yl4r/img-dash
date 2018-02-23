360fd4e84bbbaadb1f8cdc6ea0e37b21423a8d40
Tylar <murray.tylar@gmail.com>
2018-02-23 15:01:25 +0000

    update river link w/ info from @dotis email

--- assets/js/river-discharge.js
+++ assets/js/river-discharge.js
@@ -7,9 +7,9 @@ requires:
 */
 $(document).ready(function () {
     $('.river-discharge-png').each(function(){
-        // TODO: get "trinity" from http data attribute "this.data-river"
-        const filename = weekly_mean_formatter`TrinityRv_${parsed_query.date}_Discharge.png`;
-        let img_path = `http://imars-webserver-01.marine.usf.edu/modis_aqua_fgbnms/png_discharge_trinity_7d/${filename}`;
+        // TODO: get "Trinity" from http data attribute "this.data-river"
+        const filename = weekly_mean_formatter`TrinityRv_${parsed_query.date}_Discharge.jpg`;
+        let img_path = `http://imars-webserver-01.marine.usf.edu/modis_aqua_fgbnms/png_disch_7d/${filename}`;
         this.src=img_path;
     });
 });
