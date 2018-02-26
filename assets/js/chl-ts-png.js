c22ebc56e83c2975459c64505fd60a0ad7bec238
Tylar Murray <murray.tylar@gmail.com>
2018-02-26 17:04:14 +0000

    + ln to img on click

--- assets/js/chl-ts-png.js
+++ assets/js/chl-ts-png.js
@@ -10,11 +10,19 @@ $(document).ready(function () {
         const filename = this.dataset.region +
             weekly_mean_formatter`_${parsed_query.date}_CHL.jpg${false}`;
         let img_path = `http://imars-webserver-01.marine.usf.edu/modis_aqua_fgbnms/png_chl_ts_7d/${filename}`;
+        this.insertAdjacentHTML("beforeend",
+            `
+            ${this.dataset.region}
+            <a href=${img_path}>
+                <img src=${img_path} alt="[no data]">
+            </a>
+        `)
+
         this.src=img_path;
 
         const element = $(this);  // jquery element object
         element.popup({
-            html : `<img src=${img_path} width="${$(document).width()/3}">`,
+            html : `<img src=${img_path} width="${$(document).width()/2}">`,
             exclusive : true,
             variation : "flowing",
             lastResort : "top left",
