2ec62fbd513dedaea59a3bab9f0b102160667cf0
Tylar Murray <murray.tylar@gmail.com>
2018-02-26 16:39:54 +0000

    resize discharge views & + popups

--- assets/js/chl-ts-png.js
+++ assets/js/chl-ts-png.js
@@ -11,5 +11,17 @@ $(document).ready(function () {
             weekly_mean_formatter`_${parsed_query.date}_CHL.jpg${false}`;
         let img_path = `http://imars-webserver-01.marine.usf.edu/modis_aqua_fgbnms/png_chl_ts_7d/${filename}`;
         this.src=img_path;
-    });
+
+        const element = $(this);  // jquery element object
+        element.popup({
+            html : `<img src=${img_path} width="${$(document).width()/3}">`,
+            exclusive : true,
+            variation : "flowing",
+            lastResort : "top left",
+            // // none of this seems to have any affect:
+            // movePopup : false,
+            // position : "top left",
+            // setFluidWidth : false,
+        });
+    })
 });
